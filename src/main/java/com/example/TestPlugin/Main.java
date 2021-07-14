42321c17fbdaa9d4a6eb513507046b2a85b33ef6
jinsung99 <doyodo59@gmail.com>
2021-07-14 11:51:05 +0900

    initial commit

---
+++ src/main/java/com/example/TestPlugin/Main.java
@@ -0,0 +1,55 @@
+package com.example.TestPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    ConsoleCommandSender console = Bukkit.getConsoleSender();
+
+    @Override
+    public void onEnable() {
+
+        console.sendMessage(ChatColor.AQUA + "[플러그인 활성화 중 입니다.]");
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+
+        console.sendMessage( ChatColor.AQUA + "[플러그인 비활성화 중 입니다.]");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+
+        StringBuilder sb = new StringBuilder();
+        sb
+                .append(ChatColor.GREEN)
+                .append("[+] ")
+                .append(ChatColor.WHITE)
+                .append(e.getPlayer().getDisplayName());
+
+        e.setJoinMessage(  sb.toString()  );
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+
+        StringBuilder sb = new StringBuilder();
+        sb
+                .append(ChatColor.RED)
+                .append("[-] ")
+                .append(ChatColor.WHITE)
+                .append(e.getPlayer().getDisplayName());
+
+        e.setQuitMessage(  sb.toString()  );
+    }
+}
